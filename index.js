b11933351524d75a87d79c2d9fe8fe5438751aea
Montse Macias <montse.macias.u@gmail.com>
2024-09-21 14:08:56 +0200

    Ejercicio hecho

--- index.js
+++ index.js
@@ -28,17 +28,16 @@ const stringTotal = part1.substring(0 , part1.length - 1) + part1[part1.length -
 console.log(stringTotal)
 
 
-
 /*******************************************
     Iteration 2.1 | Calculate Tip
 *******************************************/
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = 84 * 0.15
 
 // Print out the tipAmount
-
+console.log(tipAmount)
 
 
 
@@ -47,10 +46,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floor(Math.random() * 10) + 1;
 
 // Print the generated random number
-
+console.log(randomNumber)
 
 
 /*******************************************
@@ -62,15 +61,29 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+console.log(expression1)
+// FALSE
 
 const expression2 = a || b;
+console.log(expression2)
+// TRUE
 
 const expression3 = !a && b;
+console.log(expression3)
+// FALSE
 
 const expression4 = !(a && b);
+console.log(expression4)
+// TRUE
 
 const expression5 = !a || !b;
+console.log(expression5)
+// TRUE
 
 const expression6 = !(a || b);
+console.log(expression6)
+// FALSE
 
 const expression7 = a && a;
+console.log(expression7)
+// TRUE
